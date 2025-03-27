f6d2778caef9d5b2ca3dc6aad6b8c82760834991
Scott M. Cullum <scott.cullum@wwt.com>
2025-03-26 21:46:38 -0400

    fix: add TypeScript definitions for Jest DOM matchers

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,9 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Ensure that the matchers are properly extended
+import { expect } from '@jest/globals';
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+// Extend Jest's expect with all the matchers from testing-library
+expect.extend(matchers);
